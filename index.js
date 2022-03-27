bb583e7f8f1f501bafd184a94c9d2c91b080e12f
Trafael Lewis <TrafLewis@gmail.com>
2022-03-27 13:49:56 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+
+    return Math.abs(pickupLocation - 42);
+
+}
+
+function distanceFromHqInFeet(pickupLocation) {
+    distanceFromHqInBlocks(pickupLocation);
+
+    return distanceFromHqInBlocks(pickupLocation) * 264;
+
+}
+
+function distanceTravelledInFeet(startBlock, endBlock) {
+
+const feetTravelled = startBlock - endBlock;
+
+return Math.abs(feetTravelled * 264);
+
+}
+
+function calculatesFarePrice(startBlock, endBlock, farePrice) {
+    distanceTravelledInFeet(startBlock, endBlock);
+
+    const feetTravelled = distanceTravelledInFeet(startBlock, endBlock) - 400;
+
+    if (feetTravelled < 1) {
+        farePrice = 0;
+    } else if ((feetTravelled >= 1) && (feetTravelled <= 1600)) {
+        farePrice = feetTravelled * 2 * 0.01;
+    } else if ((feetTravelled > 1600) && (feetTravelled <= 2100)) {
+        farePrice = 25
+    } else if (feetTravelled > 2100) {
+        return "cannot travel that far";
+    }
+
+    return farePrice;
+    }
